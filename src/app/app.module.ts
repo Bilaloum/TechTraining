e06a70c6620b630060ab551e6ccf2cf49fe9c2bb
bilaloum <bilaloumehdi01@gmail.com>
2023-05-03 07:22:21 +0100

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {  } from "swiper/angular";
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
---
+++ src/app/components/ctagories-slides/ctagories-slides.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ctagories-slides',
+  templateUrl: './ctagories-slides.component.html',
+  styleUrls: ['./ctagories-slides.component.scss'],
+})
+export class CtagoriesSlidesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+}
