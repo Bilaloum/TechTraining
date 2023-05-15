f05cc05eee4092598e8cf1b4966d1a03f3a7de80
bilaloum <bilaloumehdi01@gmail.com>
2023-05-15 21:50:23 +0100

    solve home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,6 @@ import { Course } from '../models/Course';
 import { CategoriesService } from '../Services/categories/categories.service';
 import { Category } from '../models/Category';
 import { Storage, getDownloadURL, ref } from '@angular/fire/storage';
-import { Auth } from 'firebase/auth';
 import { AuthServiceService } from '../Services/AuthServices/auth-service.service';
 import { NavController } from '@ionic/angular';
 
@@ -24,6 +23,8 @@ export class HomePage implements OnInit {
     courses: Course[];
     recomendedCourses:Course[];
     user = null ;
+    photoURL:string;
+    displayName:string;
     
     constructor( 
         private coursesService: CoursesService,
@@ -32,7 +33,7 @@ export class HomePage implements OnInit {
         private authService : AuthServiceService,
         public navCtrl: NavController,
         ) { 
-               this.InisialiseUserDate();
+                this.InisialiseUserDate();
             //categories
             this.categoriesService.getCetegories().subscribe(res => {
                 this.categories = res;
@@ -67,32 +68,22 @@ export class HomePage implements OnInit {
                 this.courses = res;
             })
         
-
     }
     
-  
     ngOnInit(): void {}
-
-  photoURL:string;
-  displayName:string;
- 
-
 
     ionViewWillEnter() {
     this.InisialiseUserDate()
-   }
+    }
 
 
-   InisialiseUserDate(){
-    console.log("initialised");
-    this.authService.getUser().subscribe((profile)=> {
-      this.displayName=profile.data().displayName?profile.data().displayName:profile.data().email.split('@')[0];
-      this.photoURL= profile.data().photoURL?profile.data().photoURL:'https://ionicframework.com/docs/img/demos/avatar.svg';
-    });
-}
+    InisialiseUserDate(){
+        this.authService.getUser().subscribe((profile)=> {
+            this.displayName=profile.data().displayName?profile.data().displayName:profile.data().email.split('@')[0];
+            this.photoURL= profile.data().photoURL?profile.data().photoURL:'https://ionicframework.com/docs/img/demos/avatar.svg';
+        });
+    }
 
-
-  
     ngAfterContentChecked(): void {
         
         this.bannerConfig ={
