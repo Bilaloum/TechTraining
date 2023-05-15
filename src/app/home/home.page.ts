6fc1eb888d70b454679a773b8a413991c9204b2e
mohammedmangari <mohammedmangari20@gmail.com>
2023-05-15 19:20:13 +0200

    refactoring of the Google Authentication service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,7 @@ import { Category } from '../models/Category';
 import { Storage, getDownloadURL, ref } from '@angular/fire/storage';
 import { Auth } from 'firebase/auth';
 import { AuthServiceService } from '../Services/AuthServices/auth-service.service';
+import { NavController } from '@ionic/angular';
 
 
 @Component({
@@ -28,8 +29,10 @@ export class HomePage implements OnInit {
         private coursesService: CoursesService,
         private categoriesService: CategoriesService,
         private fireStorage:Storage,
-        private authService : AuthServiceService
+        private authService : AuthServiceService,
+        public navCtrl: NavController,
         ) { 
+               this.InisialiseUserDate();
             //categories
             this.categoriesService.getCetegories().subscribe(res => {
                 this.categories = res;
@@ -70,7 +73,23 @@ export class HomePage implements OnInit {
   
     ngOnInit(): void {}
 
+  photoURL:string;
+  displayName:string;
+ 
 
+
+    ionViewWillEnter() {
+    this.InisialiseUserDate()
+   }
+
+
+   InisialiseUserDate(){
+    console.log("initialised");
+    this.authService.getUser().subscribe((profile)=> {
+      this.displayName=profile.data().displayName?profile.data().displayName:profile.data().email.split('@')[0];
+      this.photoURL= profile.data().photoURL?profile.data().photoURL:'https://ionicframework.com/docs/img/demos/avatar.svg';
+    });
+}
 
 
   
@@ -86,5 +105,9 @@ export class HomePage implements OnInit {
             spaceBetween:5,
         }
     }
+
+
+
+
 
 }
